56023e75fd267f52dd14335d83e8b1396a90d1d7
Evgeny39rus <denver417@gmail.com>
2023-12-29 21:35:31 +0200

    change_color_red
    Добавить в этот метод изменение цвета экрана на системный красный

--- HomeworkTen/ViewController.swift
+++ HomeworkTen/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
-}
-
+    @IBAction  func changeColor(_ sender: Any) {
+            let navBar = self.navigationController?.navigationBar
+            navBar?.barTintColor = .red
+            navBar?.isTranslucent = true
+        }
+    }
